6632206f03b960f21a10ec0189935fc1f4c9f52a
FRRitsma <folkert.ritsma@xomnia.com>
2025-01-15 20:15:40 +0100

    test script loads model

--- smaller_test_script.js
+++
@@ -1,7 +0,0 @@
-import * as ort from 'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js';
-const InferenceSession = ort.InferenceSession;
-async function main(){
-    const session = await InferenceSession.create('./onnx_model/resnet.onnx')
-}
-
-main();
