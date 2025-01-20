86a77e7ebcf8fb1c4f07942689976eef4103a842
FRRitsma <folkert.ritsma@xomnia.com>
2025-01-20 10:46:28 +0100

    added ruff and mypy cache to gitignore

--- smaller_test_script.cjs
+++ smaller_test_script.cjs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 const fs = require('fs');
-const util = require('util');
 const ort = require('onnxruntime-node');
 const path = require('path');
 const imagePath = path.join(__dirname, 'test-image-2.jpg');
@@ -14,15 +13,16 @@ const {Tensor} = require("onnxruntime-web");
 // following code also works for onnxruntime-web.
 const InferenceSession = ort.InferenceSession;
 const targetSize = 224;
+const mean = [0.485, 0.456, 0.406];
+const std = [0.229, 0.224, 0.225];
 
-//
+
 function loadLabels(labelsPath) {
     const labelsData = fs.readFileSync(labelsPath, 'utf-8');
     return JSON.parse(labelsData); // Parse and return the labels as an array
 }
 
 async function extract_image(imagePath) {
-    // Step 1: Load and resize the image to the target dimensions
     return await sharp(imagePath)
         .resize(targetSize, targetSize)
         .raw()
@@ -31,24 +31,18 @@ async function extract_image(imagePath) {
 
 // Function to preprocess the image
 function preprocessImage(imageBuffer) {
-
     const floatImage = Float32Array.from(imageBuffer, (pixel) => pixel / 255.0);
-
     // Normalize channels:
-    const mean = [0.485, 0.456, 0.406];
-    const std = [0.229, 0.224, 0.225];
     const normalizedImage = new Float32Array(floatImage.length);
     for (let i = 0; i < floatImage.length; i++) {
         const channel = i % 3; // Assuming RGB with 3 channels
         normalizedImage[i] = (floatImage[i] - mean[channel]) / std[channel];
     }
-
     //Rearrange dimensions to [C, H, W] from [H, W, C]
     const channels = 3;
     const height = targetSize;
     const width = targetSize;
     const transposedImage = new Float32Array(normalizedImage.length);
-
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
             for (let c = 0; c < channels; c++) {
@@ -60,7 +54,6 @@ function preprocessImage(imageBuffer) {
 
     // Add a batch dimension to create a tensor of shape [1, C, H, W]
     const tensor = new Tensor('float32', transposedImage, [1, 3, height, width]);
-
     return tensor;
 }
 
@@ -68,26 +61,19 @@ function preprocessImage(imageBuffer) {
 async function getPrediction(session, imagePath, labelsPath) {
     try {
         const imageBuffer = await extract_image(imagePath);
-
         // Preprocess the image
         const inputTensor = preprocessImage(imageBuffer);
-
         // Get the model's input name
         const inputName = session.inputNames[0];
-
         // Run inference
         const results = await session.run({ [inputName]: inputTensor });
-
         // Extract the output tensor
         const outputName = session.outputNames[0];
         const scores = results[outputName].data; // Float32Array
-
         // Find the index of the highest value
         const maxIndex = scores.indexOf(Math.max(...scores));
-
         // Load class labels
         const labels = loadLabels(labelsPath);
-
         // Get the label for the highest index
         const predictedLabel = labels[maxIndex];
 
@@ -101,7 +87,6 @@ async function getPrediction(session, imagePath, labelsPath) {
 
 // use an async context to call onnxruntime functions.
 async function main() {
-
     try {
         const session01 = await InferenceSession.create('./onnx_model/resnet.onnx');
         const results = await getPrediction(session01, imagePath, labelsPath);
